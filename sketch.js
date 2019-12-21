8330bd30cbcc05bb6ffb83660f521657436a81cf
kimtaeill <57379411+kimtaeill@users.noreply.github.com>
2019-12-21 21:40:34 +0900

    Fix Assets location

--- sketch.js
+++ sketch.js
@@ -12,21 +12,22 @@ let img6 ;
 let img7 ;
 
 function preload(){
-  bgm = loadSound("bgm.mp3");
-  img1 = loadImage('puzzle0.png');
-  img2 = loadImage('puzzle00.png');
-  img3 = loadImage('puzzle1.png');
-  img4 = loadImage('puzzle2.png');
-  img5 = loadImage('puzzle3.png');
-  img6 = loadImage('puzzle4.png');
-  img7 = loadImage('puzzle5.png');
+  bgm = loadSound("assets/bgm.mp3");
+  img1 = loadImage('assets/puzzle0.png');
+  img2 = loadImage('assets/puzzle00.png');
+  img3 = loadImage('assets/puzzle1.png');
+  img4 = loadImage('assets/puzzle2.png');
+  img5 = loadImage('assets/puzzle3.png');
+  img6 = loadImage('assets/puzzle4.png');
+  img7 = loadImage('assets/puzzle5.png');
 }
 
 function setup() {
-  bgm.play();
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
   noCursor();
+  bgm.play();
+  bgm.loop();
 }
 
 function draw() {
